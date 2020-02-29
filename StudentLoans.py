32c70228ae433c5e5ca9709731bac73e297e3f17
tracychen112 <38146302+tracychen112@users.noreply.github.com>
2020-02-29 00:50:31 -0500

    Add files via upload

--- StudentLoans.py
+++ StudentLoans.py
@@ -13,8 +13,33 @@ def simpleIntRate(principal,rate,years):
     # assuming month is 30 day cycle
     month_pay = daily_int*30
     year_int = month_pay*12
-    return year_int
+    return year_int*years
 
-def compoundIntRate(principal,rate,years):
+def compoundIntRate(principal,rate,years): 
+
+
+
+# yearVect is a vector of possible years to repay the loan default/baseline is 10
+# we can also add to the length of th vector too 
+def amortizedStudentLoan(principal,rate,yearVect,fedLoan):
+    loan_projections = []
+    amt = 0
+    total = 0
+    annuity = 0
+    # first value in yearVect should be defaul 10 years
+    for i in range(0,len(yearVect)): 
+        # its a federal loan
+        if (fedLoan):
+            amt = simpleIntRate(principal,rate,yearVect[i])
+            
+        else:
+            amt = compoundIntRate(principal,rate,yearVect[i])
+        
+        total = principal + amt
+        # subtracted 1 because you don't pay when 
+        annuity = total/((yearVect[i]*12)-1)
+        loan_projections.append(annuity)
+    
+    
        
     
